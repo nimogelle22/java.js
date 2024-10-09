c39dcbe5ec28d453392f7b1e67ca232e187a10e9
nimogelle22 <nimogorgor@gmail.com>
2024-10-09 10:46:25 +0300

    done

---
+++ java.js
@@ -0,0 +1,31 @@
+let nimo ="hello world";
+console.log(nimo);
+
+
+let myname ;
+myname ="I am nimo";
+console.log(myname);
+
+let gele ="welcome";
+console.log(gele);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// declaration variable
+    //  var
+    // let
+    //  const
+// variable name
+// datatype
+// value
+
